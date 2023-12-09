4c8bb725ac674c8f1eb005dc11201b3f558b8538
Leva_Dokhoyan <leva.dokhoyan@contracted.pmru.com>
2023-12-09 20:32:46 +0400

    Cosmetic changes

--- src/main/java/com/testmaster/service/impl/UserServiceImpl.java
+++ src/main/java/com/testmaster/service/impl/UserServiceImpl.java
@@ -15,11 +15,15 @@ public class UserServiceImpl implements UserService {
     @NonNull
     private final InOutService inOutService;
 
+    private static final String ASK_NAME_MESSAGE = "Your name: ";
+    private static final String ASK_SURNAME_MESSAGE = "Your surname: ";
+
+
     @Override
     public User initUser() {
-        inOutService.printMessage("Your name: ");
+        inOutService.printMessage(ASK_NAME_MESSAGE);
         String name = inOutService.readLine();
-        inOutService.printMessage("Your surname: ");
+        inOutService.printMessage(ASK_SURNAME_MESSAGE);
         String surname = inOutService.readLine();
 
         return new User(name, surname);

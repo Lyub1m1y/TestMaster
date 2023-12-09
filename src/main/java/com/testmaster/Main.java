3eadf7b9b9c8345fdd9173bbcd5bc9f8be3ae4dd
Leva_Dokhoyan <leva.dokhoyan@contracted.pmru.com>
2023-12-09 20:29:23 +0400

    Cosmetic changes

--- src/main/java/com/testmaster/Main.java
+++ src/main/java/com/testmaster/Main.java
@@ -2,14 +2,17 @@ package com.testmaster;
 
 import com.testmaster.config.AppConfig;
 import com.testmaster.launcher.TestLauncher;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
   public static void main(String[] args) {
     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     TestLauncher launcher = context.getBean(TestLauncher.class);
 
-    launcher.startApp();
+    log.info("App started!");
+    launcher.run();
   }
 }
--- src/main/java/com/testmaster/launcher/TestLauncher.java
+++ src/main/java/com/testmaster/launcher/TestLauncher.java
@@ -11,13 +11,11 @@ import java.util.List;
 import com.testmaster.service.UserService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
-@Slf4j
 public class TestLauncher {
 
   @NonNull
@@ -29,26 +27,31 @@ public class TestLauncher {
   @NonNull
   private final TestResultConverter testResultConverter;
 
-  public void startApp() {
-    log.info("App started!");
+  private static final String EMPTY_MESSAGE = "";
+  private static final String TEST_NOT_FOUND_MESSAGE = "Test not found.";
+  private static final String NO_AVAILABLE_TESTS_MESSAGE = "There are no tests available to take.";
+  private static final String AVAILABLE_TESTS_MESSAGE = "Available tests:";
+  private static final String SELECT_TEST_MESSAGE = "Select a test: ";
+
+  public void run() {
     try {
       User user = userService.initUser();
-      inOutService.printMessage("");
+      inOutService.printMessage(EMPTY_MESSAGE);
 
       List<String> availableTests = testService.getAvailableTests();
       if (!availableTests.isEmpty()) {
         displayAvailableTests(availableTests);
-        inOutService.printMessage("");
+        inOutService.printMessage(EMPTY_MESSAGE);
         UserTest selectedTest = selectTest();
         if (selectedTest != null) {
           TestResult testResult = testService.performTest(selectedTest);
           testResult.setUser(user);
           inOutService.printMessage(testResultConverter.convert(testResult));
         } else {
-          inOutService.printMessage("Test not found.");
+          inOutService.printMessage(TEST_NOT_FOUND_MESSAGE);
         }
       } else {
-        inOutService.printMessage("There are no tests available to take.");
+        inOutService.printMessage(NO_AVAILABLE_TESTS_MESSAGE);
       }
     } catch (Exception ex) {
       inOutService.printMessage(ex.getMessage());
@@ -56,14 +59,14 @@ public class TestLauncher {
   }
 
   private void displayAvailableTests(List<String> availableTests) {
-    inOutService.printMessage("Available tests:");
+    inOutService.printMessage(AVAILABLE_TESTS_MESSAGE);
     for (String testName : availableTests) {
       inOutService.printMessage(testName);
     }
   }
 
   private UserTest selectTest() {
-    inOutService.printMessage("Select a test: ");
+    inOutService.printMessage(SELECT_TEST_MESSAGE);
     String testName = inOutService.readLine();
     return testService.getTestByName(testName);
   }
